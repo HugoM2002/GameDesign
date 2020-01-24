0ef96236f53e35701e43e73786305bcad72e1089
Hugo Moritz <529282@student.glu.nl>
2020-01-24 09:22:45 +0100

    Begonnen
    -Enemy
    -Player
    -Wolf

---
+++ GamedesignHugoTom/Assets/Script/Enemy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    public GameObject _target = null;
+    private NavMeshAgent _navMeshAgent = null;
+    [SerializeField] public float _health = 15f;
+    [SerializeField] public float _speed = 6f;
+    [SerializeField] public float _damage = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _navMeshAgent.SetDestination(_target.transform.position);
+    }
+}
---
+++ GamedesignHugoTom/Assets/Script/Player.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float _health = 10f;
+    [SerializeField] private float _damage = 5f;
+    [SerializeField] private float _sideWaysForce = 10f;
+    [SerializeField] private float _forwardBackForce = 10f;
+    private Rigidbody _rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("w"))
+        {
+            _rb.AddForce(0, 0, _forwardBackForce * Time.deltaTime);
+        }
+        if (Input.GetKey("s"))
+        {
+            _rb.AddForce(0, 0, -_forwardBackForce * Time.deltaTime);
+        }
+        if (Input.GetKey("a"))
+        {
+            _rb.AddForce(0, 0, -_sideWaysForce * Time.deltaTime);
+        }
+        if (Input.GetKey("d"))
+        {
+            _rb.AddForce(0, 0, _sideWaysForce * Time.deltaTime);
+        }
+    }
+}
